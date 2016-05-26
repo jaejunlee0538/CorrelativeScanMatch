44c76bb9bd9cd79dfe4d14682905bacad64889b5
jaejunlee0538 <jaejun0201@gmail.com>
2016-05-27 02:18:35 +0900

    rotation

--- octave/correlativeScanMatch.m
+++ octave/correlativeScanMatch.m
@@ -30,61 +30,59 @@ YTick = pixelsToPoints([YTick;YTick], cellSize, Xmin, Ymin, Xmax, Ymax);
 set(gca, 'XTickLabel', XTick(1,:));
 set(gca, 'YTickLabel', YTick(2,:));
 %%
-% thetas = deg2rad([-1.0:0.25:1.0]);
+Theta0 = theta;
 X0 = 0;
 Y0 = 0;
 
-xy2_r = v2t([0 0 theta]) * xy2;
-% xy2_r = v2t(dpos_true) * xy2;
-N_xy2 = size(xy2_r,2);
 %%
 figure(1); hold off;
 plot(xy1(1,:),xy1(2,:),'r.');
 hold on;
-plot(xy2_r(1,:), xy2_r(2,:),'b.');
+plot(xy2(1,:), xy2(2,:),'b.');
 legend('scan1','scan2')
 grid on; axis equal
 %%
+gridTheta = [-0.34:0.075:0.34] + Theta0;
 gridX = [0,-2.0:cellSize:2.0] + X0;
 gridY = [0,-2.0:cellSize:2.0] + Y0;
-% gridX = [-0.1];
-% gridY = [-0.05];
-probMap = zeros(length(gridX), length(gridY));
+
 scores = [];
 best_score = -10000;
 best_align = [0 0];
-for ix = 1:length(gridX)
-    for iy = 1:length(gridY)
-        tmp = xy2_r(1:2,:) + repmat([gridX(ix);gridY(iy)], 1, N_xy2);
-%         figure(1);plot(tmp(1,:),tmp(2,:),'m.'); drawnow; return;
-        lookup = pointsToPixels(tmp, cellSize, Xmin, Ymin, Xmax, Ymax);
-        lookup(:,lookup(1,:)<1 | lookup(2,:)<1 | lookup(1,:)>size(lookUpTable,1)...
-            | lookup(2,:)>size(lookUpTable,2)) = [];
+for itheta = 1:length(gridTheta)
+    probMap = zeros(length(gridX), length(gridY));
+    xy2_r = v2t([0 0 gridTheta(itheta)]) * xy2;
+    for ix = 1:length(gridX)
+        for iy = 1:length(gridY)
+            tmp = xy2_r(1:2,:) + repmat([gridX(ix);gridY(iy)], 1, size(xy2_r,2));
+    %         figure(1);plot(tmp(1,:),tmp(2,:),'m.'); drawnow; return;
+            lookup = pointsToPixels(tmp, cellSize, Xmin, Ymin, Xmax, Ymax);
+            lookup(:,lookup(1,:)<1 | lookup(2,:)<1 | lookup(1,:)>size(lookUpTable,1)...
+                | lookup(2,:)>size(lookUpTable,2)) = [];
 
-        if ~isempty(lookup)
-            scanInd = lookup(1,:) + (lookup(2,:) - 1).*size(lookUpTable,1);
-            prob = sum(lookUpTable(scanInd));
-            if best_score < prob
-                best_score = prob;
-                best_align = [gridX(ix) gridY(iy)];
-                
-                figure(2);
-                imagesc(lookUpTable'); hold on; 
-                set(gca,'YDir','normal'); axis equal;
-                plot(lookup(1,:), lookup(2,:),'ro'); hold off; drawnow;
+            if ~isempty(lookup)
+                scanInd = lookup(1,:) + (lookup(2,:) - 1).*size(lookUpTable,1);
+                prob = sum(lookUpTable(scanInd));
+                if best_score < prob
+                    best_score = prob;
+                    best_align = [gridX(ix) gridY(iy)];
+
+                    figure(2);
+                    imagesc(lookUpTable'); hold on; 
+                    set(gca,'YDir','normal'); axis equal;
+                    plot(lookup(1,:), lookup(2,:),'ro'); hold off; drawnow;
+                end
+            else
+                prob = 0;
             end
-        else
-            prob = 0;
-        end
-        
-        probMap(ix, iy) = prob;
-    end
-end
-best_align
-figure(3);
-imagesc(probMap');
-set(gca, 'YDir','normal');
 
+            probMap(ix, iy) = prob;
+        end
+    end
+    figure(3);
+    imagesc(probMap');
+    set(gca, 'YDir','normal');
+end
 figure(4);
 figure(4); hold off;
 plot(xy1(1,:),xy1(2,:),'r.');
